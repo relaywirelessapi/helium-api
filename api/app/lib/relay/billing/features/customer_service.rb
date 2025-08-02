c29fa4f8cd2ef90f5eb5e83ff7ba180821b0a571
Alessandro Desantis <desa.alessandro@gmail.com>
2025-08-02 18:06:31 +0200

    Improve modeling of customer service plan feature

--- api/app/lib/relay/billing/features/customer_service.rb
+++ api/app/lib/relay/billing/features/customer_service.rb
@@ -1,37 +1,57 @@
-# typed: false
+# typed: strict
 
 module Relay
   module Billing
     module Features
       class CustomerService < Feature
+        class Tier
+          extend T::Sig
+
+          class << self
+            extend T::Sig
+
+            sig { returns(Tier) }
+            def community
+              new(name: "Community support", description: "Community-based support in our Discord.")
+            end
+
+            sig { returns(Tier) }
+            def business
+              new(name: "Business support", description: "Direct support from the Relay team.")
+            end
+          end
+
+          sig { returns(String) }
+          attr_reader :name
+
+          sig { returns(String) }
+          attr_reader :description
+
+          sig { params(name: String, description: String).void }
+          def initialize(name:, description:)
+            @name = name
+            @description = description
+          end
+        end
+
         extend T::Sig
 
-        sig { returns(Symbol) }
+        sig { returns(Tier) }
         attr_reader :tier
 
-        sig { params(tier: Symbol).void }
+        sig { params(tier: Tier).void }
         def initialize(tier:)
           @tier = tier
         end
 
         sig { override.returns(String) }
         def name
-          case tier
-          when :community
-            "Community support"
-          when :business
-            "Standard support"
-          end
+          tier.name
         end
 
         sig { override.returns(String) }
         def description
-          case tier
-          when :community
-            "Community-based support in our Discord."
-          when :business
-            "Direct support from Relay team."
-          end
+          tier.description
         end
       end
     end
